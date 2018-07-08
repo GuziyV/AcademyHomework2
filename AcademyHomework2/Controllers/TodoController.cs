49edf56e90a0096787ef96a5c147c74c7a46b6ca
GuziyV <volodyaguziy3@gmail.com>
2018-07-08 23:33:47 +0300

    hotfix(change routing)

---
+++ AcademyHomework2/Controllers/TodoController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AcademyHomework2.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AcademyHomework2.Controllers
+{
+    public class TodoController : Controller
+    {
+        UserService userService;
+
+        public TodoController()
+        {
+            userService = new UserService();
+        }
+        //GET: User/GetTodo/{id}
+        public IActionResult GetTodo(int id)
+        {
+            var todo = userService.GetTodoById(id);
+            if (todo == null)
+            {
+                return new NotFoundResult();
+            }
+            else
+            {
+                ViewBag.User = userService.GetUserById(todo.UserId);
+                return View(todo);
+            }
+        }
+    }
+}
